1c7204d2ac0267b1cf737dbac7c5e81d3b865837
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-05 20:30:00 +0200

    importing AnecdoteList and moving related logic to its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { handleVote } from './reducers/anecdoteReducer'
+
+import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const sortedAnecdotes=anecdotes.toSorted((a,b)=>b.votes-a.votes)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(handleVote(id))
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new</h2>
+      <AnecdoteList />
+    
       <AnecdoteForm />
       
     </div>
