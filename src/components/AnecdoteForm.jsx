4643e06db1101390b297ec22789671fb1a44200c
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-05 20:15:49 +0200

    separating form component to its own module

---
+++ src/components/AnecdoteForm.jsx
@@ -0,0 +1,21 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+const AnecdoteForm = () => {
+  const dispatch=useDispatch()
+  const addAnecdote= (event) => {
+    event.preventDefault()
+    const anecdote=event.target.input1.value
+    event.target.input1.value=''
+    dispatch(createAnecdote(anecdote))
+
+  }
+  return(
+    <form onSubmit={addAnecdote}>
+        <div><input name='input1'/></div>
+        <button type='submit'>create</button>
+      </form>
+  )
+
+}
+
+export default AnecdoteForm
