4ad7e67891efe56034561bef867f39c14c0a5eef
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-05 18:20:15 +0200

    importing configureStore and creating store for app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
+import rootReducer from './reducers/anecdoteReducer'
 
-const store = createStore(reducer)
+const store = configureStore({reducer:rootReducer})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
