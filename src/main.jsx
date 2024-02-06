deef2d1ae6ec020c7f9b708f657cb217a24d1315
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-06 18:50:52 +0200

    importing and using combineReducers to implement multiple reducer in a store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,combineReducers } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import rootReducer from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = configureStore({reducer:rootReducer})
+const reducer = combineReducers({
+  anecdotes:anecdoteReducer,
+  textfilter:filterReducer
+})
+
+const store = configureStore({ reducer })
+console.log(store.getState());
+//const store = configureStore({reducer:anecdoteReducer})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
