72158ade5a907769bcc74f9dc7ce486ada811586
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-06 19:13:20 +0200

    using configureStore to declare multiple reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import ReactDOM from 'react-dom/client'
-import { configureStore,combineReducers } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
+
+
+const store = configureStore({ reducer:{
   anecdotes:anecdoteReducer,
   textfilter:filterReducer
-})
-
-const store = configureStore({ reducer })
+} })
 console.log(store.getState());
 //const store = configureStore({reducer:anecdoteReducer})
 
