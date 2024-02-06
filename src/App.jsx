5d9210944310e271ecfa8fb9d742a2bb0847c427
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-06 18:52:30 +0200

    Adding FilterForm component inside main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import FilterForm from './components/FilterForm'
 
 const App = () => {
 
   return (
     <div>
       <h2>Anecdotes</h2>
+      < FilterForm />
       <AnecdoteList />
-    
       <AnecdoteForm />
       
     </div>
