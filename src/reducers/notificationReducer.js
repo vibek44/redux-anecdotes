f7742e864cf288e9b349e6581c7348eefd9737b8
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-08 20:11:30 +0200

    creating notification reducer and action creator using createSlice

---
+++ src/reducers/notificationReducer.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name:'notification',
+  initialState:'',
+  reducers:{
+    setNotification(state,action){
+      console.log(typeof action.payload.id);
+      
+      if(action.payload.id===undefined && typeof action.payload!==Object)
+      return state=`you added ' ${action.payload}' `
+      if( typeof action.payload.id==='string')
+      return state=`you voted ' ${action.payload.content}' `
+    },
+    removeNotification(state,action){
+     return state=action.payload
+    },
+  }
+})
+
+export const { setNotification,removeNotification }=notificationSlice.actions
+export default notificationSlice.reducer
