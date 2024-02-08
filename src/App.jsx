90ab2b2dd05d56032a4b0e2b26a5128de2ca68c7
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-08 20:16:12 +0200

    importing Notification component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Notification from './components/Notification'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import FilterForm from './components/FilterForm'
@@ -7,6 +7,7 @@ const App = () => {
 
   return (
     <div>
+      <Notification />
       <h2>Anecdotes</h2>
       < FilterForm />
       <AnecdoteList />
