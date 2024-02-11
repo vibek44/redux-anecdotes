9c2d641a1d26d665df43d7bb7acbfeb98a34e5f0
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-11 16:43:45 +0200

    changing addAnecdote function to async to do post async operation

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,14 +1,17 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification,removeNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdote'
 const AnecdoteForm = () => {
   const dispatch=useDispatch()
-  const addAnecdote= (event) => {
+  
+  const addAnecdote= async(event) => {
     event.preventDefault()
     const anecdote=event.target.input1.value
     event.target.input1.value=''
-    dispatch(createAnecdote(anecdote))
-    dispatch(setNotification(anecdote))
+    const newAnecdote=await anecdoteService.create(anecdote)
+    dispatch(createAnecdote(newAnecdote))
+    dispatch(setNotification(newAnecdote.content))
     setTimeout(()=>{
       dispatch(removeNotification(''))
     },5000)
