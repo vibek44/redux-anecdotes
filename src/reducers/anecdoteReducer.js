497489cbf2419ed819c4032651abf6984ff86bc3
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-12 17:45:28 +0200

    using thunk to do update asynchronous operation update, with addVote thunk

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,52 +1,55 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdote'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-//map method is used to create initial state passed to reducer from array of anecdotesAtStart
-const initialState = anecdotesAtStart.map(asObject)
 
 const anecdoteSlice=createSlice({
   name:'anecdote',
-  initialState,
+  initialState:[],
   reducers:{
-    createAnecdote(state,action){
-      const content=action.payload
-      state.push({
-        content,
-        id:getId(),
-        votes:0
-      })
-    },
-    //console.log(JSON.parse(JSON.stringify(state)))
-    voteAnecdote(state,action){
-      const id=action.payload
-      const anecdoteToVote=state.find(anecdote =>anecdote.id===id )
-      const votedAnecdote = {
-        ...anecdoteToVote,
-        votes:anecdoteToVote.votes+1
-      }
-      return state.map(anecdote => anecdote.id !== id ? anecdote :votedAnecdote)
+    //console.log(JSON.parse(JSON.stringify(state))) to print state in redux
+    voteAnecdotee(state,action){
+      const votedAnecdote=action.payload
+      return state.map(anecdote => anecdote.id !== votedAnecdote.id ? anecdote :votedAnecdote)
 
+    },
+    appendAnecdote(state,action){
+        state.push(action.payload)
+    },
+    setAnecdote(state,action){
+      return action.payload
     },
   }
 })
-export const { createAnecdote, voteAnecdote}=anecdoteSlice.actions
+export const { voteAnecdotee,appendAnecdote,setAnecdote}=anecdoteSlice.actions
+
+export const initializeAnecdote=() => {
+  return async dispatch => {
+    const anecdotes=await anecdoteService.getAll()
+    dispatch(setAnecdote(anecdotes))
+  }
+}
+
+export const createAnecdote=(content) => {
+  return async dispatch => {
+    const newAnecdote=await anecdoteService.create(content)
+    dispatch(appendAnecdote(newAnecdote))
+  }
+}
+
+export const addVote=(id)=>{
+  return async (dispatch,getState) =>{
+    //console.log(id,getState());
+    let changedAnecdote=getState().anecdotes.find(anecdote=>anecdote.id===id)
+    changedAnecdote={...changedAnecdote,votes:changedAnecdote.votes+1}
+    await anecdoteService.update(changedAnecdote)
+    //console.log(changedAnecdote);
+    
+    dispatch(voteAnecdotee(changedAnecdote))
+  }
+}
+
+
 export default anecdoteSlice.reducer
 
 
